279a4f6505057c6bf9acb18a77fc4b4fdc98aea3
denverma7 <126649187+denverma7@users.noreply.github.com>
2025-07-04 20:56:00 +0500

    Changes express config

--- index.js
+++ index.js
@@ -117,7 +117,7 @@
 
 require('dotenv').config();
 
-const express Sara = require('express');
+const express = require('express');
 const cors = require("cors");
 const app = express();
 const mongoose = require('mongoose');
